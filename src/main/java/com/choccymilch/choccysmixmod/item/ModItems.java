e44ab97664d782b7575abfd39b39ddafec4c5aa9
IAmChoccyMilch <rabbitwow8@gmail.com>
2025-08-28 17:12:43 -0400

    Cheese is now edible
    Added cheese_curd.png and cheese_curd.json

--- src/main/java/com/choccymilch/choccysmixmod/item/ModItems.java
+++ src/main/java/com/choccymilch/choccysmixmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.choccymilch.choccysmixmod.item;
 
 import com.choccymilch.choccysmixmod.ChoccysMixMod;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -9,9 +10,16 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ChoccysMixMod.MOD_ID);
 
-    public static final DeferredItem<Item> CHEESE = ITEMS.registerItem("cheese", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> CHEESE = ITEMS.registerSimpleItem("cheese", new Item.Properties().food(new FoodProperties.Builder()
+                    // Heals 3 hearts.
+                    .nutrition(3)
+                    .saturationModifier(0.3f)
+                    .alwaysEdible().build()
+            )
+    );
 
     public static final DeferredItem<Item> CURD = ITEMS.registerItem("cheese_curd", Item::new, new Item.Properties());
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
